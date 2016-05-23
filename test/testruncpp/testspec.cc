60c35507046d2d1ba3d78f729dc9fb2f02e45c84
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2016-05-23 00:29:06 -0700

    move things around

--- test/testruncpp/testspec.cc
+++ test/testruncpp/testspec.cc
@@ -1,9 +1,9 @@
-#include <cstddef>
 #include "spec.h"
 #include "gtest/gtest.h"
+#include <cstddef>
 
-std::string test_file = "../../../test/test.json";
-runcpp::spec::Spec spec(test_file);
+std::string test_dir = "../../../test";
+runcpp::spec::Spec spec(test_dir);
 
 TEST(test_spec, test_spec_version) { EXPECT_EQ("0.5.0-dev", spec.oci_version); }
 
